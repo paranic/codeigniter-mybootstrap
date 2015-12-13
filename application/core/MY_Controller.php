1442008bd08750001c062e9f87b407d7d794dea7
paranic <paranic@gmail.com>
2015-12-13 14:41:11 -0500

    Added MY_Controller witch i will extend later.

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller {
+
+	public $view_data = [];
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+}
+
+/* End of file MY_Controller.php */
+/* Location: ./application/core/MY_Controller.php */
